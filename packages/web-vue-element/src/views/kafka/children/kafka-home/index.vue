9d88de74463ae931436ee806cbf104f5a0f59af7
ZRMYDYCG <547471919@qq.com>
2025-01-12 15:04:01 +0800

    feat: Kafka 集群概览页适配多屏幕断点

--- packages/web-vue-element/src/views/kafka/children/kafka-home/index.vue
+++ packages/web-vue-element/src/views/kafka/children/kafka-home/index.vue
@@ -2,29 +2,30 @@
 </script>
 
 <template>
-  <div class="kafka-home">
+  <div class="kafka-home p-4">
+    <!-- Kafka 集群概览 -->
     <div>
       <h1 class="text-nowrap text-lg font-bold text-gray-700">Kafka 集群概览</h1>
       <hr class="my-2" />
-      <div class="flex items-center justify-between w-full h-full border border-gray-200 rounded-md p-3">
-        <div>
+      <div class="flex flex-col md:flex-row items-center justify-between w-full h-full border border-gray-200 rounded-md p-3">
+        <div class="mb-2 md:mb-0 flex items-center justify-center">
           <span>在线运行时长:</span>
-          <span>12 小时 30 分钟</span>
+          <span class="ml-2">12 小时 30 分钟</span>
         </div>
-        <div class="grid grid-cols-2 grid-rows-2 gap-2">
-          <div>
+        <div class="grid grid-cols-2 grid-rows-2 gap-2 w-full md:w-auto flex justify-center"> <!-- 横向居中 -->
+          <div class="flex items-center justify-center">
             <span>在线节点个数:</span>
             <span class="text-red-700 ml-2">3个</span>
           </div>
-          <div>
+          <div class="flex items-center justify-center">
             <span>在线zk数:</span>
             <span class="text-red-700 ml-2">3个</span>
           </div>
-          <div>
+          <div class="flex items-center justify-center">
             <span>离线节点数:</span>
             <span class="text-red-700 ml-2">0个</span>
           </div>
-          <div>
+          <div class="flex items-center justify-center">
             <span>离线zk数:</span>
             <span class="text-red-700 ml-2">0个</span>
           </div>
@@ -32,10 +33,11 @@
       </div>
     </div>
 
-    <div>
-      <h1 class="text-nowrap text-lg font-bold text-gray-700 mt-2">Kafka 节点检测</h1>
+    <!-- Kafka 节点检测 -->
+    <div class="mt-6">
+      <h1 class="text-nowrap text-lg font-bold text-gray-700">Kafka 节点检测</h1>
       <hr class="my-2" />
-      <div class="grid grid-cols-5 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         <div class="flex flex-col gap-2 border border-gray-200 rounded-md p-3" v-for="(item, index) in 40" :key="index">
           <div>
             <span>节点域名:</span>
@@ -59,10 +61,11 @@
       </div>
     </div>
 
-    <div>
-      <h1 class="text-nowrap text-lg font-bold text-gray-700 mt-2">Zookeeper 连通性</h1>
+    <!-- Zookeeper 连通性 -->
+    <div class="mt-6">
+      <h1 class="text-nowrap text-lg font-bold text-gray-700">Zookeeper 连通性</h1>
       <hr class="my-2" />
-      <div class="grid grid-cols-5 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         <div class="flex flex-col gap-2 border border-gray-200 rounded-md p-3" v-for="(item, index) in 40" :key="index">
           <div>
             <span>节点域名:</span>
