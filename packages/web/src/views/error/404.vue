c1fbccfd1ba0a8f5cc3772f05184f4cba47ee8b7
ZRMYDYCG <547471919@qq.com>
2025-02-05 11:55:00 +0800

    feat: 404页面

--- packages/web/src/views/error/404.vue
+++ packages/web/src/views/error/404.vue
@@ -1,11 +1,21 @@
-<template>
-  <div>
-    <h1>404 - 页面未找到</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-<script>
-export default {
-  name: 'NotFound',
+const router = useRouter()
+
+const handleGoHome = () => {
+  router.push('/')
 }
 </script>
+
+<template>
+  <el-empty
+    class="flex justify-center items-center h-screen w-screen"
+    image="https://github.com/user-attachments/assets/6ffce1db-db9a-4243-b5ae-eec7354c696e"
+    description="没有找到该页面, 请检查地址是否正确"
+  >
+    <el-button type="primary" size="small" @click="handleGoHome"
+      >返回首页</el-button
+    >
+  </el-empty>
+</template>
