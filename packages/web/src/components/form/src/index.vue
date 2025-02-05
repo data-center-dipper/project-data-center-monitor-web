8f59097df0b78fdf1d99f240db62828b947593cb
ZRMYDYCG <547471919@qq.com>
2025-02-06 00:22:48 +0800

    feat: json 驱动视图, 支持所有表单控件

--- packages/web/src/components/form/src/index.vue
+++ packages/web/src/components/form/src/index.vue
@@ -1,5 +1,224 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, watch } from "vue"
+import type { RuleItem } from "./rule.ts"
+import cloneDeep from "lodash/cloneDeep"
+import type { CSSProperties } from "vue"
 
-<template></template>
+interface FormOptions {
+  // 表单项显示的元素
+  type:
+    | "cascader"
+    | "checkbox"
+    | "checkbox-group"
+    | "checkbox-button"
+    | "radio"
+    | "radio-group"
+    | "radio-button"
+    | "color-picker"
+    | "date-picker"
+    | "input"
+    | "input-number"
+    | "rate"
+    | "select"
+    | "option"
+    | "slider"
+    | "switch"
+    | "time-picker"
+    | "time-select"
+    | "transfer"
+    | "upload"
+  // 表单项的值
+  value?: any
+  // 表单的Label
+  label?: string
+  // 表单的标识
+  prop?: string
+  // 表单的验证规则
+  rules?: RuleItem
+  // 表单项的提示信息
+  placeholder?: string
+  // 表单元素特有的属性
+  attrs?: {
+    clearable?: boolean
+    showPassword?: boolean
+    disabled?: boolean
+    // css 样式
+    style?: CSSProperties
+  }
+  // 表单项的子元素
+  children?: FormOptions[]
+  // 处理上传组件的属性和方法
+  uploadAttrs?: {
+    action: string
+    headers?: object
+    method?: "post" | "put" | "patch"
+    multiple?: boolean
+    data?: any
+    name?: string
+    withCredentials?: boolean
+    showFileList?: boolean
+    drag?: boolean
+    accept?: string
+    thumbnailMode?: boolean
+    fileList?: any[]
+    listType?: "text" | "picture" | "picture-card"
+    autoUpload?: boolean
+    disabled?: boolean
+    limit?: number
+  }
+}
+
+const props = defineProps<{
+  // 表单项配置
+  options: FormOptions[]
+  // 用户自定义的文件上传方法
+  httRequest?: () => void
+}>()
+
+const emits = defineEmits([
+  "on-preview",
+  "on-remove",
+  "on-success",
+  "on-error",
+  "on-progress",
+  "on-change",
+  "before-remove",
+  "before-upload",
+  "on-exceed",
+])
+
+const form = ref<any>(null)
+
+const model = ref(null)
+const rules = ref({})
+
+const initForm = () => {
+  if (props.options && props.options.length) {
+    let m: any = {}
+    let r: any = {}
+    props.options.map((item: FormOptions) => {
+      m[item.prop] = item.value
+      r[item.prop] = item.rules
+    })
+    model.value = cloneDeep(m)
+    rules.value = cloneDeep(r)
+  }
+}
+
+watch(
+  () => props.options,
+  () => {
+    initForm()
+  },
+  { deep: true },
+)
+
+const onPreview = (file: any) => {
+  emits("on-preview", file)
+}
+
+const onRemove = (file: any, fileList: any) => {
+  emits("on-remove", { file, fileList })
+}
+
+const onSuccess = (response: any, file: any, fileList: any) => {
+  // 文件上传成功后, 给表单项进行赋值
+  let uploadItem = props.options.find((item) => item.type === "upload")
+  model.value[uploadItem.prop] = { response, file, fileList }
+  emits("on-success", { response, file, fileList })
+}
+
+const onError = (err: any, file: any, fileList: any) => {
+  emits("on-error", { err, file, fileList })
+}
+
+const onProgress = (event: any, file: any, fileList: any) => {
+  emits("on-progress", { event, file, fileList })
+}
+
+const onChange = (file: any, fileList: any) => {
+  emits("on-change", { file, fileList })
+}
+
+const beforeRemove = (file: any, fileList: any) => {
+  return emits("before-remove", { file, fileList })
+}
+
+const beforeUpload = (file: any) => {
+  return emits("before-upload", file)
+}
+
+const onExceed = (files: any, fileList: any) => {
+  emits("on-exceed", { files, fileList })
+}
+
+onMounted(() => {
+  initForm()
+})
+</script>
+
+<template>
+  <el-form
+    :validate-on-rule-change="false"
+    v-bind="$attrs"
+    :model="model"
+    :rules="rules"
+    v-if="model"
+    ref="form"
+  >
+    <template v-for="(item, index) in options" :key="index">
+      <el-form-item
+        :label="item.label"
+        :prop="item.prop"
+        v-if="!item.children || !item.children.length"
+      >
+        <component
+          v-if="item.type !== 'upload'"
+          v-bind="item.attrs"
+          :is="`el-${item.type}`"
+          v-model="model[item.prop]"
+        ></component>
+        <el-upload
+          v-bind="item.uploadAttrs"
+          :on-preview="onPreview"
+          :on-remove="onRemove"
+          :on-success="onSuccess"
+          :on-error="onError"
+          :on-progress="onProgress"
+          :on-change="onChange"
+          :before-remove="beforeRemove"
+          :before-upload="beforeUpload"
+          :on-exceed="onExceed"
+          v-else
+        >
+          <!--     文件上传触发区域     -->
+          <slot name="uploadTrigger"> </slot>
+          <!--     文件上传提示信息     -->
+          <slot name="uploadTip"> </slot>
+        </el-upload>
+      </el-form-item>
+      <el-form-item v-if="item.children && item.children.length">
+        <component
+          v-bind="item.attrs"
+          :is="`el-${item.type}`"
+          v-model="model[item.prop]"
+        >
+          <component
+            v-for="(child, index) in item.children"
+            :key="index"
+            :label="child.label"
+            :value="child.value"
+            :is="`el-${child.type}`"
+          >
+          </component>
+        </component>
+      </el-form-item>
+    </template>
+    <el-form-item>
+      <!--   将表单实例通过作用域插槽暴露给外部调用其实例方法以及表单收集到的数据   -->
+      <slot name="actions" :form="form" :model="model"></slot>
+    </el-form-item>
+  </el-form>
+</template>
 
 <style scoped></style>
