02a9b0887bc153dec862d95412962a38ad6e0c6d
541711153@qq.com <541711153@qq.com>
2025-02-22 14:43:54 +0800

    feat: 消费者界面tab页展示

--- packages/web/src/views/kafka/children/kafka-consumer/components/ConsumerItem.vue
+++ packages/web/src/views/kafka/children/kafka-consumer/components/ConsumerItem.vue
@@ -1,44 +1,32 @@
 <template>
-  <div class="consumer-item">
-    <div class="consumer-header">
+  <el-card class="consumer-item">
+    <div slot="header" class="clearfix">
       <span>{{ consumer.topic }}</span>
       <span>{{ consumer.groupId }}</span>
       <span>{{ consumer.businessProperty }}</span>
       <span>{{ consumer.lastUpdateTime }}</span>
       <span>{{ consumer.deadline }}</span>
-      <a href="#" @click.prevent="refresh(consumer)">刷新</a>
-      <a href="#" @click.prevent="update(consumer)">更新</a>
-      <a href="#" @click.prevent="remove()">删除</a>
+      <el-link type="primary" :underline="false" @click.prevent="refresh(consumer)">刷新</el-link>
+      <el-link type="primary" :underline="false" @click.prevent="update(consumer)">更新</el-link>
+      <el-link type="primary" :underline="false" @click.prevent="remove()">删除</el-link>
     </div>
     <!-- 默认展开的消费者详情 -->
     <div class="consumer-details">
-      <table class="details-table">
-        <thead>
-          <tr>
-            <th>topic名称</th>
-            <th>分区</th>
-            <th>数据起点offset</th>
-            <th>当前读取offset</th>
-            <th>数据结尾offset</th>
-            <th>延迟lag</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="detail in consumer.details" :key="detail.partition">
-            <td>{{ detail.topicName }}</td>
-            <td>{{ detail.partition }}</td>
-            <td>{{ detail.startOffset }}</td>
-            <td>{{ detail.currentOffset }}</td>
-            <td>{{ detail.endOffset }}</td>
-            <td>{{ detail.lag }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <el-table :data="consumer.details" style="width: 100%">
+        <el-table-column prop="topicName" label="topic名称"></el-table-column>
+        <el-table-column prop="partition" label="分区"></el-table-column>
+        <el-table-column prop="startOffset" label="数据起点offset"></el-table-column>
+        <el-table-column prop="currentOffset" label="当前读取offset"></el-table-column>
+        <el-table-column prop="endOffset" label="数据结尾offset"></el-table-column>
+        <el-table-column prop="lag" label="延迟lag"></el-table-column>
+      </el-table>
     </div>
-  </div>
+  </el-card>
 </template>
 
 <script setup>
+import { defineProps, defineEmits } from 'vue';
+
 const props = defineProps({
   consumer: Object,
 });
@@ -59,5 +47,25 @@ function remove() {
 </script>
 
 <style scoped>
-/* 样式根据需要自行定义 */
+.consumer-item {
+  margin-bottom: 20px;
+}
+
+.clearfix {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.clearfix span {
+  margin-right: 10px;
+}
+
+.el-link {
+  margin-left: 10px;
+}
+
+.consumer-details {
+  padding-top: 15px;
+}
 </style>
