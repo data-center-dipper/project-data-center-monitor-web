c7b34a9c9b92317c16908560bfff1e97d2340f95
ZRMYDYCG <547471919@qq.com>
2025-02-24 21:06:21 +0800

    feat: 新增Icon组件,基于font-awesome图标库开发

---
+++ src/components/Icon/src/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { IconProps } from './interface.ts'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { omit } from 'lodash-es'
+import { computed } from 'vue'
+
+defineOptions({
+  name: 'qx-icon',
+  inheritAttrs: false,
+})
+
+const props = withDefaults(defineProps<IconProps>(), {})
+
+const filterProps = computed(() => omit(props, ['type', 'color']))
+const customStyle = computed(() => {
+  return {
+    color: props.color ?? void 0,
+  }
+})
+</script>
+
+<template>
+  <i
+    class="bre-icon inline-flex justify-center items-center relative"
+    :style="customStyle"
+    v-bind="$attrs"
+  >
+    <font-awesome-icon v-bind="filterProps" />
+  </i>
+</template>
+
+<style scoped>
+.bre-icon {
+  fill: currentColor;
+  font-size: inherit;
+}
+</style>
---
+++ src/views/kafka/children/kafka-consumer/components/consumer-content.vue
@@ -0,0 +1,55 @@
+<template>
+  <ConsumerDetail
+    v-for="(consumer, index) in consumers"
+    :key="index"
+    :consumer="consumer"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import ConsumerDetail from './consumer-detail.vue'
+
+const props = defineProps<{
+  selectedTopics: string
+  selectedConsumerGroups: string
+}>()
+
+const consumers = ref<any[]>([
+  {
+    topic: 'Topic名称1',
+    groupId: '消费组A1',
+    businessProperty: '业务属性1',
+    lastUpdateTime: new Date().toLocaleString(),
+    deadline: '2025-01-01',
+    isDelayed: true,
+    avgConsumeRate: '100eps',
+  },
+  {
+    topic: 'Topic名称1',
+    groupId: '消费组A1',
+    businessProperty: '业务属性1',
+    lastUpdateTime: new Date().toLocaleString(),
+    deadline: '2025-01-01',
+    isDelayed: true,
+    avgConsumeRate: '100eps',
+  },
+  {
+    topic: 'Topic名称1',
+    groupId: '消费组A1',
+    businessProperty: '业务属性1',
+    lastUpdateTime: new Date().toLocaleString(),
+    deadline: '2025-01-01',
+    isDelayed: true,
+    avgConsumeRate: '100eps',
+  },
+])
+
+watch(
+  () => [props.selectedTopics, props.selectedConsumerGroups],
+  async ([newTopic, newGroup]) => {},
+  { immediate: true },
+)
+</script>
+
+<style scoped></style>
