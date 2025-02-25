c91de780f3115aff886dae8b8d44a09c43bf5c03
ZRMYDYCG <547471919@qq.com>
2025-02-25 21:10:30 +0800

    feat: Topic指标组

--- src/views/kafka/children/kafka-topic-load/components/select-indicator-information.vue
+++ src/views/kafka/children/kafka-topic-load/components/select-indicator-information.vue
@@ -1,5 +1,37 @@
-<script setup lang="ts"></script>
+<template>
+  <qx-card class="mt-3">
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox
+        v-for="option in options"
+        :key="option.value"
+        :label="option.label"
+        :value="option.value"
+      />
+    </el-checkbox-group>
+  </qx-card>
+</template>
 
-<template></template>
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import QxCard from '@/components/Card/src/index.vue'
 
-<style scoped></style>
+const emits = defineEmits(['update:indicator'])
+
+const checkList = ref(['cpu_usage', 'memory_usage'])
+
+const options = [
+  { label: 'CPU 使用率', value: 'cpu_usage' },
+  { label: '内存使用情况', value: 'memory_usage' },
+  { label: '磁盘使用率', value: 'disk_usage' },
+  { label: '网络带宽', value: 'network_bandwidth' },
+  { label: '进程监控', value: 'process_monitoring' },
+  { label: '服务可用性', value: 'service_availability' },
+  { label: '日志监控', value: 'log_monitoring' },
+  { label: '异常警报', value: 'anomaly_alerts' },
+  { label: '应用性能', value: 'application_performance' },
+]
+
+watch(checkList, (newVal) => {
+  emits('update:indicator', newVal)
+})
+</script>
--- src/views/kafka/children/kafka-topic-load/index.vue
+++ src/views/kafka/children/kafka-topic-load/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="mt-3">
     <TopicLoadHeader></TopicLoadHeader>
+    <SelectIndicatorInformation
+      @update:indicator="handleUpdateIndicator"
+    ></SelectIndicatorInformation>
+    <HostMonitoringTable></HostMonitoringTable>
+    <ChartPreview></ChartPreview>
   </div>
 </template>
 
@@ -9,4 +14,8 @@ import TopicLoadHeader from './components/topic-load-header.vue'
 import SelectIndicatorInformation from './components/select-indicator-information.vue'
 import HostMonitoringTable from './components/host-monitoring-table.vue'
 import ChartPreview from './components/chart-preview.vue'
+
+function handleUpdateIndicator(indicator) {
+  console.log(indicator)
+}
 </script>
