bf1104f1354f8f45cc3fc0ca643e710056d6cb08
ZRMYDYCG <547471919@qq.com>
2025-02-25 21:17:27 +0800

    feat: 依赖注入指标

--- src/views/kafka/children/kafka-topic-load/index.vue
+++ src/views/kafka/children/kafka-topic-load/index.vue
@@ -9,13 +9,18 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref, provide } from 'vue'
 import TopicLoadHeader from './components/topic-load-header.vue'
 import SelectIndicatorInformation from './components/select-indicator-information.vue'
 import HostMonitoringTable from './components/host-monitoring-table.vue'
 import ChartPreview from './components/chart-preview.vue'
 
-function handleUpdateIndicator(indicator) {
-  console.log(indicator)
+const indicator = ref([])
+
+function handleUpdateIndicator(newIndicator) {
+  indicator.value = newIndicator
 }
+
+provide('indicator', indicator)
 </script>
